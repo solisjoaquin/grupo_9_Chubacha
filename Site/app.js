861c19fd9dae9d0dd136cf985e81629a9e1c4141
Carlitos7Almeida <carlitos7almeida@gmail.com>
2020-06-26 23:05:02 -0300

    Se agregó carpeta Design y crado rutas y archivos html

--- Site/app.js
+++ Site/app.js
@@ -4,22 +4,22 @@ const app = express();
 
 //Pedido a pagina Home
 app.get('/', (req, res) => {
-    res.send('Probando123')
+    res.sendFile(__dirname + '/views/index.html')
 });
 
 //Pedido a pagina Productos
 app.get('/Productos', (req, res) => {
-    res.send('Probando 123')
+    res.sendFile(__dirname + '/views/Productos.html' )
 });
 
 //Pedido a pagina Carrito de Compras
 app.get('/Carrito', (req, res) => {
-    res.send('Probando 123')
+    res.sendFile(__dirname + '/views/Carrito.html')
 });
 
 //Pedido a pagina Registro y Login
 app.get('/RegistroLogin', (req, res) => {
-    res.send('Probando 123')
+    res.sendFile(__dirname + '/views/RegistroLogin.html')
 });
 
 app.listen(3000, () => {

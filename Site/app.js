1e019d4c73968759b77789998dd81fd11ab067d8
eogieglo <eric.ogieglo@gmail.com>
2020-07-25 23:01:49 -0300

    Modifique las rutas del app.js

--- Site/app.js
+++ Site/app.js
@@ -8,18 +8,24 @@ app.get('/', (req, res) => {
 });
 
 //Pedido a pagina Productos
-app.get('/Productos', (req, res) => {
-    res.sendFile(__dirname + '/views/Productos.html' )
+app.get('/producto', (req, res) => {
+    res.sendFile(__dirname + '/views/producto.html' )
 });
 
 //Pedido a pagina Carrito de Compras
-app.get('/Carrito', (req, res) => {
-    res.sendFile(__dirname + '/views/Carrito.html')
+app.get('/carrito', (req, res) => {
+    res.sendFile(__dirname + '/views/carrito.html')
 });
 
 //Pedido a pagina Registro y Login
-app.get('/RegistroLogin', (req, res) => {
-    res.sendFile(__dirname + '/views/RegistroLogin.html')
+app.get('/registro', (req, res) => {
+    res.sendFile(__dirname + '/views/registro.html')
+});
+
+
+//Pedido a pagina Registro y Login
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
 });
 
 app.listen(3000, () => {

c6f6c8f82f8f59d49c75f1d044ae073c2043dee4
eogieglo <eric.ogieglo@gmail.com>
2020-09-14 22:09:51 -0300

    ADD Structure SQL

--- Site/data/structure.sql
+++ Site/data/structure.sql
@@ -8,19 +8,54 @@ use chubachaBD;
 
 ---  TABLE category_user-------
 
-create table category_user(id int auto_increment primary key, category_name varchar(50));
+create table category_user(
+    id int auto_increment primary key, 
+    category_name varchar(50)
+    );
 
 ---  TABLE category -----
 
-create table category(id int auto_increment primary key, name varchar(50));
+create table category(
+    id int auto_increment primary key, 
+    name varchar(50)
+    );
 
 ---- TABLE user  ------
 
-create table user(id int auto_increment primary key, name varchar(50),last_name varchar (50), email varchar(50), password varchar (50), avatar varchar(50),category_id int, foreign key (category_id) references category_user (id));
+create table user(
+    id int auto_increment primary key, 
+    name varchar(50),
+    last_name varchar (50), 
+    email varchar(50), 
+    password varchar (50), 
+    avatar varchar(50),
+    category_id int, 
+    foreign key (category_id) 
+        references category_user (id)
+    );
 
 ---- TABLE products -----
 
-create table products(id int auto_increment primary key, name varchar(50),description varchar (50), cost varchar (50), image varchar(50),category_id int, foreign key (category_id) references category(id));
+create table products(
+    id int auto_increment primary key, 
+    name varchar(50),
+    description varchar (50), 
+    cost varchar (50), 
+    image varchar(50),
+    category_id int, 
+    foreign key (category_id) 
+        references category(id)
+    );
 
 ---  TABLE purchase  -----
-create table purchase(id int auto_increment primary key, user_id int,product_id int, date date, precio_total int, foreign key (user_id) references user(id), foreign key (product_id) references products(id));
+create table purchase(
+    id int auto_increment primary key, 
+    user_id int,
+    product_id int, 
+    date date, 
+    precio_total int, 
+    foreign key (user_id) 
+        references user(id), 
+    foreign key (product_id) 
+        references products(id)
+    );
